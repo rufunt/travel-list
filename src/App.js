27a800a5ff5245f1b4090b325d337d91cc572325
rufunt <jazzoo@mail.ru>
2024-01-18 13:17:57 +0300

    Add basic componets

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴🌴🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for you 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have x items</em>
+    </footer>
+  );
 }
